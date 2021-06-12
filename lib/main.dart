aa0bbf9f18d52d145b4c84dcfb24b5d07a2deec4
Nuttapon <nuddev@gmail.com>
2021-06-12 21:48:59 +0700

    Change lib  webview

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:webview/pages/splash_page.dart';
-import 'package:webview/widgets/basic_webview.dart';
-import 'package:webview_flutter/webview_flutter.dart';
 
 void main() {
   runApp(MyApp());
--- lib/widgets/basic_webview.dart
+++ lib/widgets/basic_webview.dart
@@ -3,8 +3,8 @@ import 'dart:io';
 import 'package:device_info/device_info.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 import 'package:webview/config/BuildConfig.dart';
-import 'package:webview_flutter/webview_flutter.dart';
 
 class BasicWebview extends StatefulWidget {
   @override
@@ -12,14 +12,12 @@ class BasicWebview extends StatefulWidget {
 }
 
 class _BasicWebviewState extends State<BasicWebview> {
-  WebViewController _webView;
   DeviceInfoPlugin deviceInfoPlugin = DeviceInfoPlugin();
   String _mobileVersion;
-
+  final flutterWebviewPlugin = new FlutterWebviewPlugin();
   @override
   void initState() {
     super.initState();
-    if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
     initPlatformState();
   }
 
@@ -42,11 +40,9 @@ class _BasicWebviewState extends State<BasicWebview> {
   }
 
   Future<bool> _handleOnBack() async {
-    var value = await _webView.canGoBack();
-    var currentUrl = await _webView.currentUrl();
-    print(currentUrl);
-    if (value) {
-      _webView.goBack();
+  var canGoBack = await  flutterWebviewPlugin.canGoBack();
+    if (canGoBack) {
+      flutterWebviewPlugin.goBack();
       return false;
     }
     return false;
@@ -54,21 +50,17 @@ class _BasicWebviewState extends State<BasicWebview> {
 
   @override
   Widget build(BuildContext context) {
+  var url =  '${BuildConfig.WEBVIEW_URL}?wvAuth=${BuildConfig.PACKAGE_NAME}&key=${BuildConfig.KEY}&os=${Platform.operatingSystem}&mobileVersion=${_mobileVersion}';
     return WillPopScope(
       onWillPop: _handleOnBack,
       child: Scaffold(
         body: SafeArea(
-          child: _mobileVersion == null
-              ? Container()
-              : WebView(
-                  onWebViewCreated: (WebViewController webViewController) {
-                    _webView = webViewController;
-                  },
-                  javascriptMode: JavascriptMode.unrestricted,
-                  initialUrl:
-                      '${BuildConfig.WEBVIEW_URL}?wvAuth=${BuildConfig.PACKAGE_NAME}&key=${BuildConfig.KEY}&os=${Platform.operatingSystem}&mobileVersion=${_mobileVersion}',
-                ),
-        ),
+            child: _mobileVersion == null
+                ? Container()
+                : WebviewScaffold(
+                    hidden: false,
+                    url: url,
+                  )),
       ),
     );
   }
